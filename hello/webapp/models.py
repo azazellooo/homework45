6ccf3613113329e48e3f2adfb8e7350592314436
karlygach <karygachkenenbaeva@gmail.com>
2021-02-24 12:43:06 +0600

    Создана модель Tasks

--- hello/webapp/models.py
+++ hello/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Task(models.Model):
+    description = models.CharField(max_length=160, null=False, blank=False)
+    status = models.CharField(max_length=100, null=False, blank=False)
+    date = models.DateField(null=True)
+
+    class Meta:
+        db_table = 'tasks'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return f'{self.id}. {self.description}: {self.date}'
 # Create your models here.
