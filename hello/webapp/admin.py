bfcdeb53161a2a232e0560554f6b7f2eabef200e
karlygach <karygachkenenbaeva@gmail.com>
2021-02-25 15:38:48 +0600

    добавлена модель Task, миграция, регистрация в админке

--- hello/webapp/admin.py
+++ hello/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from webapp.models import Task
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'date', 'status']
+    list_filter = ['date', 'status']
+    search_fields = ['description']
+    fields = ['id', 'status', 'description', 'date']
+    readonly_fields = ['status', 'id']
+
+
+admin.site.register(Task, TaskAdmin)
 # Register your models here.
--- hello/webapp/models.py
+++ hello/webapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Task(models.Model):
+    status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
     description = models.CharField(max_length=160, null=False, blank=False)
-    status = models.CharField(max_length=100, null=False, blank=False)
+    status = models.CharField(max_length=100, choices=status_choices, default='new')
     date = models.DateField(null=True)
 
     class Meta:
