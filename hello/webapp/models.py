6a997fb0694ea3f4a7bcbb2bcd2245a7f57c8b5f
karlygach <karygachkenenbaeva@gmail.com>
2021-02-26 15:15:09 +0600

    изменена модель task

--- hello/webapp/models.py
+++ hello/webapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
 
 class Task(models.Model):
-    status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
     description = models.CharField(max_length=160, null=False, blank=False)
+    text = models.TextField(max_length=1500, null=True, blank=True)
     status = models.CharField(max_length=100, choices=status_choices, default='new')
     date = models.DateField(null=True, blank=True)
 
